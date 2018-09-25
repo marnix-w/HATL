0e2a9ece71f18235f900db9c7ac98bcb801a5070
jpaardekooper <p_jazzzp@live.nl>
2018-09-25 21:01:26 +0200

    added random json compiler

---
+++ HotelSimulationTheLock/Assets/Json/JsonSerializer.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelSimulationTheLock.Assets.Json
+{
+    class JsonSerializer
+    {
+    }
+}
+
+// To parse this JSON data, add NuGet 'Newtonsoft.Json' then do:
+//
+//    using QuickType;
+//
+//    var welcome = Welcome.FromJson(jsonString);
+
+//namespace QuickType
+//{
+//    using System;
+//    using System.Collections.Generic;
+
+//    using System.Globalization;
+//    using Newtonsoft.Json;
+//    using Newtonsoft.Json.Converters;
+
+//    public partial class Welcome
+//    {
+//        [JsonProperty("AreaType")]
+//        public AreaType AreaType { get; set; }
+
+//        [JsonProperty("Position")]
+//        public string Position { get; set; }
+
+//        [JsonProperty("Dimention")]
+//        public Dimention Dimention { get; set; }
+
+//        [JsonProperty("Capacity", NullValueHandling = NullValueHandling.Ignore)]
+//        public long? Capacity { get; set; }
+
+//        [JsonProperty("Classification", NullValueHandling = NullValueHandling.Ignore)]
+//        public string Classification { get; set; }
+//    }
+
+//    public enum AreaType { Cinema, Restaurant, Room };
+
+//    public enum Dimention { The11, The21, The22 };
+
+//    public partial class Welcome
+//    {
+//        public static Welcome[] FromJson(string json) => JsonConvert.DeserializeObject<Welcome[]>(json, QuickType.Converter.Settings);
+//    }
+
+//    public static class Serialize
+//    {
+//        public static string ToJson(this Welcome[] self) => JsonConvert.SerializeObject(self, QuickType.Converter.Settings);
+//    }
+
+//    internal static class Converter
+//    {
+//        public static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
+//        {
+//            MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
+//            DateParseHandling = DateParseHandling.None,
+//            Converters = {
+//                AreaTypeConverter.Singleton,
+//                DimentionConverter.Singleton,
+//                new IsoDateTimeConverter { DateTimeStyles = DateTimeStyles.AssumeUniversal }
+//            },
+//        };
+//    }
+
+//    internal class AreaTypeConverter : JsonConverter
+//    {
+//        public override bool CanConvert(Type t) => t == typeof(AreaType) || t == typeof(AreaType?);
+
+//        public override object ReadJson(JsonReader reader, Type t, object existingValue, JsonSerializer serializer)
+//        {
+//            if (reader.TokenType == JsonToken.Null) return null;
+//            var value = serializer.Deserialize<string>(reader);
+//            switch (value)
+//            {
+//                case "Cinema":
+//                    return AreaType.Cinema;
+//                case "Restaurant":
+//                    return AreaType.Restaurant;
+//                case "Room":
+//                    return AreaType.Room;
+//            }
+//            throw new Exception("Cannot unmarshal type AreaType");
+//        }
+
+//        public override void WriteJson(JsonWriter writer, object untypedValue, JsonSerializer serializer)
+//        {
+//            if (untypedValue == null)
+//            {
+//                serializer.Serialize(writer, null);
+//                return;
+//            }
+//            var value = (AreaType)untypedValue;
+//            switch (value)
+//            {
+//                case AreaType.Cinema:
+//                    serializer.Serialize(writer, "Cinema");
+//                    return;
+//                case AreaType.Restaurant:
+//                    serializer.Serialize(writer, "Restaurant");
+//                    return;
+//                case AreaType.Room:
+//                    serializer.Serialize(writer, "Room");
+//                    return;
+//            }
+//            throw new Exception("Cannot marshal type AreaType");
+//        }
+
+//        public static readonly AreaTypeConverter Singleton = new AreaTypeConverter();
+//    }
+
+//    internal class DimentionConverter : JsonConverter
+//    {
+//        public override bool CanConvert(Type t) => t == typeof(Dimention) || t == typeof(Dimention?);
+
+//        public override object ReadJson(JsonReader reader, Type t, object existingValue, JsonSerializer serializer)
+//        {
+//            if (reader.TokenType == JsonToken.Null) return null;
+//            var value = serializer.Deserialize<string>(reader);
+//            switch (value)
+//            {
+//                case "1, 1":
+//                    return Dimention.The11;
+//                case "2, 1":
+//                    return Dimention.The21;
+//                case "2, 2":
+//                    return Dimention.The22;
+//            }
+//            throw new Exception("Cannot unmarshal type Dimention");
+//        }
+
+//        public override void WriteJson(JsonWriter writer, object untypedValue, JsonSerializer serializer)
+//        {
+//            if (untypedValue == null)
+//            {
+//                serializer.Serialize(writer, null);
+//                return;
+//            }
+//            var value = (Dimention)untypedValue;
+//            switch (value)
+//            {
+//                case Dimention.The11:
+//                    serializer.Serialize(writer, "1, 1");
+//                    return;
+//                case Dimention.The21:
+//                    serializer.Serialize(writer, "2, 1");
+//                    return;
+//                case Dimention.The22:
+//                    serializer.Serialize(writer, "2, 2");
+//                    return;
+//            }
+//            throw new Exception("Cannot marshal type Dimention");
+//        }
+
+//        public static readonly DimentionConverter Singleton = new DimentionConverter();
+//    }
+//}
