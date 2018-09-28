d0b5f1e2d93d6f68235b7bb17d6d854b0d070add
Marnix Wieffering <17020379@student.hhs.nl>
2018-09-28 14:58:06 +0200

    added area factory

--- HotelSimulationTheLock/Model/Hotel.cs
+++ HotelSimulationTheLock/Model/Hotel.cs
@@ -4,12 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HotelSimulationTheLock.Factories
+namespace HotelSimulationTheLock.Model
 {
-    class AreaFactory
+    class Hotel
     {
-
-       
-
     }
 }
