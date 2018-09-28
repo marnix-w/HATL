29e9148992af977e146fb5b74d3f604c543fdad0
Marnix Wieffering <17020379@student.hhs.nl>
2018-09-28 15:18:24 +0200

    factory

--- HotelSimulationTheLock/Factories/AreaFactory.cs
+++ HotelSimulationTheLock/Factories/AreaFactory.cs
@@ -1,15 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HotelSimulationTheLock.Factories
+namespace HotelSimulationTheLock
 {
-    class AreaFactory
+    public enum AreaType
     {
+        Room,
+        Cinema,
+        Fitness,
+        Pool,
+        Reception,
+        Elevator,
+        Staircase
+    }
 
-       
-
+    class AreaFactory 
+    {
+        public static IArea GetArea(AreaType typeOfArea, int capicity, Point position, Point dimention, int amountOfStars)
+        {
+            switch (typeOfArea)
+            {
+                case AreaType.Room:
+                    return new Room();
+                case AreaType.Cinema:
+                    return new Cinema();
+                case AreaType.Fitness:
+                    return new Fitness();
+                case AreaType.Pool:
+                    return new Pool();
+                case AreaType.Reception:
+                    return new Reception();
+                case AreaType.Elevator:
+                    return new Elevator();
+                case AreaType.Staircase:
+                    return new Staircase();
+                default:
+                    return null;
+                   
+            }
+            
+        }
+      
     }
 }
