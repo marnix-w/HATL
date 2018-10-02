d3049f0b5c34d5285c79a7302d10b9c3f6188904
jpaardekooper <p_jazzzp@live.nl>
2018-10-02 14:24:15 +0200

    fullsize hotel room

--- HotelSimulationTheLock/Simulation.cs
+++ HotelSimulationTheLock/Simulation.cs
@@ -15,6 +15,7 @@ namespace HotelSimulationTheLock
     public partial class Simulation : Form
     {
         public Hotel HotelArea { get; set; }
+        PictureBox Background = new PictureBox();
 
         public Simulation(List<JsonModel> layout)
         {
@@ -27,18 +28,18 @@ namespace HotelSimulationTheLock
             HotelEventManager.Start();
             Console.WriteLine(!HotelEventManager.Running);
 
-
-
-            showHotelAreaOverView();
+            ShowHotelAreaOverView();
 
             HotelEventManager.HTE_Factor = 0.5f;
 
         }
 
-        public void showHotelAreaOverView()
+        public void ShowHotelAreaOverView()
         {
             foreach (IArea i in HotelArea.HotelAreaList)
             {
+                // DrawHotelToBitmap(i.Art, i.Position.X * 96, i.Position.Y * 96, i.Dimension.X * 96, i.Dimension.Y * 96);
+
                 _eventsOutput.Text += i.Position;
                 _eventsOutput.Text += i.Status;
                 _eventsOutput.Text += i.Dimension;
@@ -51,31 +52,44 @@ namespace HotelSimulationTheLock
                 test.Location = new Point(i.Position.X * 96, i.Position.Y * 96);
                 test.Width = i.Dimension.X * 96;
                 test.Height = i.Dimension.Y * 96;
+                test.SizeMode = PictureBoxSizeMode.StretchImage;
                 test.Image = i.Art;
+
                 this.Controls.Add(test);
             }
 
-          
+
+
+
             for (int i = 1; i < 8; i++)
             {
                 //elavator
                 PictureBox elevator = new PictureBox();
                 elevator.Location = new Point(0, i * 96);
-                elevator.Image = Properties.Resources.elevator_not_pressent;
-                elevator.Width = 96;
-                elevator.Height = 96;
+                if (i == 7)
+                {
+                    elevator.Image = Properties.Resources.elevator_pressent;
+                }
+                else
+                {
+                    elevator.Image = Properties.Resources.elevator_not_pressent;
+                }
+                elevator.SizeMode = PictureBoxSizeMode.AutoSize;
                 this.Controls.Add(elevator);
 
                 //staircase
                 PictureBox staircase = new PictureBox();
-                staircase.Location = new Point(9 * 96 , i * 96);
+                staircase.Location = new Point(9 * 96, i * 96);
                 staircase.Image = Properties.Resources.staircase;
-                staircase.Width = 96;
-                staircase.Height = 96;
+                staircase.SizeMode = PictureBoxSizeMode.AutoSize;
                 this.Controls.Add(staircase);
             }
 
+
+
         }
+
+
 
     }
 }
