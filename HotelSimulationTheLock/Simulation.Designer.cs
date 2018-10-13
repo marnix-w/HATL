d88f24ffbb7b70e5aa800a9fa115ea50342ed6e1
jpaardekooper <p_jazzzp@live.nl>
2018-10-13 16:54:36 +0200

    changed layout of simulation

--- HotelSimulationTheLock/Simulation.Designer.cs
+++ HotelSimulationTheLock/Simulation.Designer.cs
@@ -33,38 +33,42 @@
             this.guestTB = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // roomTB
             // 
-            this.roomTB.Location = new System.Drawing.Point(993, 362);
+            this.roomTB.Location = new System.Drawing.Point(50, 383);
             this.roomTB.Multiline = true;
             this.roomTB.Name = "roomTB";
             this.roomTB.ReadOnly = true;
-            this.roomTB.Size = new System.Drawing.Size(348, 361);
+            this.roomTB.Size = new System.Drawing.Size(350, 392);
             this.roomTB.TabIndex = 6;
             // 
             // facillityTB
             // 
-            this.facillityTB.Location = new System.Drawing.Point(993, 738);
+            this.facillityTB.Location = new System.Drawing.Point(50, 791);
             this.facillityTB.Multiline = true;
             this.facillityTB.Name = "facillityTB";
             this.facillityTB.ReadOnly = true;
-            this.facillityTB.Size = new System.Drawing.Size(348, 121);
+            this.facillityTB.Size = new System.Drawing.Size(350, 141);
             this.facillityTB.TabIndex = 7;
             // 
             // guestTB
             // 
-            this.guestTB.Location = new System.Drawing.Point(993, 89);
+            this.guestTB.Location = new System.Drawing.Point(50, 100);
             this.guestTB.Multiline = true;
             this.guestTB.Name = "guestTB";
             this.guestTB.ReadOnly = true;
-            this.guestTB.Size = new System.Drawing.Size(348, 267);
+            this.guestTB.Size = new System.Drawing.Size(350, 267);
             this.guestTB.TabIndex = 8;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(354, 873);
+            this.button1.Location = new System.Drawing.Point(728, 29);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 10;
@@ -81,11 +85,51 @@
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(522, 821);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(86, 68);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(743, 821);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(86, 68);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(944, 821);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(86, 68);
+            this.button5.TabIndex = 14;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1346, 121);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "simInfo";
+            // 
             // Simulation
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1676, 976);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.guestTB);
@@ -105,5 +149,9 @@
         private System.Windows.Forms.TextBox guestTB;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label1;
     }
 }
--- HotelSimulationTheLock/Simulation.cs
+++ HotelSimulationTheLock/Simulation.cs
@@ -42,7 +42,7 @@ namespace HotelSimulationTheLock
 
             HotelBackground = new PictureBox
             {
-                Location = new Point(50, 100),
+                Location = new Point(450, 100),
                 Width = Hotel.HotelWidth * RoomArtSize,
                 Height = Hotel.HotelHeight * RoomArtSize,
                 SizeMode = PictureBoxSizeMode.StretchImage,
@@ -100,7 +100,7 @@ namespace HotelSimulationTheLock
                 }
                 catch (Exception e)
                 {
-                    Debug.WriteLine("jasper fix je stats shit" + e.Message);
+                    Debug.WriteLine(e.Message);
                 }
             }
         }
@@ -130,6 +130,7 @@ namespace HotelSimulationTheLock
             {
                 _pauseResume = true;
                 button1.Text = "Resume";
+                label1.Text = "Simulation has been paused";
                 t.Stop();
                 HotelEventManager.Stop();
             }
@@ -137,6 +138,7 @@ namespace HotelSimulationTheLock
             {
                 _pauseResume = false;
                 button1.Text = "Pause";
+                label1.Text = "Simulation is running on normaal speed";
                 t.Start();
                 HotelEventManager.Start();
             }
